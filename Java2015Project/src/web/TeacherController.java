8c24b6b3d09f29189582d806b733518c938c874c
LucasLibis <lucas.libis@hotmail.fr>
2015-04-02 13:44:40 +0200

    Début de la partie Teacher

---
+++ Java2015Project/src/web/TeacherController.java
@@ -0,0 +1,21 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TeacherController extends HttpServlet
+{
+	public void init()
+	{
+		
+	}
+	
+	public void service(HttpServletRequest req, HttpServletResponse rep) throws ServletException, IOException
+	{
+		
+	}
+}
