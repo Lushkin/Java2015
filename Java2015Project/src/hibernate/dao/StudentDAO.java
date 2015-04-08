f6d948ec5c432b4bf2a6e94cdcadbeee1a384c57
LucasLibis <lucas.libis@hotmail.fr>
2015-04-08 16:05:33 +0200

    Amélioration Student

--- Java2015Project/src/hibernate/dao/StudentDAO.java
+++ Java2015Project/src/hibernate/dao/StudentDAO.java
@@ -4,6 +4,8 @@ import hibernate.HibernateUtil;
 import hibernate.java.Tests;
 import hibernate.java.UserTests;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Hibernate;
@@ -11,16 +13,20 @@ import org.hibernate.Session;
 
 public class StudentDAO
 {
+	@SuppressWarnings("unchecked")
 	public List<UserTests> GetPendingTests(int studentId)
 	{
 		Session session;
 		try
 		{
+			SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy");
+			String today = formater.format(new Date());
 			session = HibernateUtil.currentSession();
-			List<UserTests> tests = session.createQuery("from UserTests Where UserId = " + studentId + " AND State = 0").list();
+			List<UserTests> tests = session.createQuery("select ut from UserTests ut join ut.tests t Where UserId = " + studentId + " AND State = 0 AND t.startDate < GETDATE() AND t.endDate > GETDATE()").list();
 			for(UserTests t : tests)
 			{
 				Hibernate.initialize(t.getTests());
+				Hibernate.initialize(t.getTests().getSubjects());
 			}
 			HibernateUtil.closeSession();
 			
@@ -37,10 +43,12 @@ public class StudentDAO
 		try
 		{
 			session = HibernateUtil.currentSession();
-			List<UserTests> tests = session.createQuery("from UserTests Where UserId = " + studentId + " AND State = 1").list();
+			//Object toto = session.createQuery("select endDate from Tests WHERE endDate < GETDATE()").list();
+			List<UserTests> tests = session.createQuery("select ut from UserTests ut join ut.tests t Where userId = " + studentId  + " AND (state = 1 OR t.endDate < GETDATE())").list();
 			for(UserTests t : tests)
 			{
 				Hibernate.initialize(t.getTests());
+				Hibernate.initialize(t.getTests().getSubjects());
 			}
 			HibernateUtil.closeSession();
 			
